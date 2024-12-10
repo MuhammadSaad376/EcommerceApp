83d3ca1c6284a2da93805ffcdd884c1e3b7ada60
MuhammadSaad376 <saad.akbar@bracketmind.com>
2024-12-10 18:10:35 +0500

    EcomerceApp Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat'; // Ensure the /compat version is used
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment.development';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavbarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule, // Add AngularFireAuth if using authentication
+    AngularFirestoreModule, // Add Firestore if using the Firestore database
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAOgFTgSzLfKdkcjCM7h3Xar403zqEQfr8',
+    authDomain: 'ecommerceshop-223b3.firebaseapp.com',
+    projectId: 'ecommerceshop-223b3',
+    storageBucket: 'ecommerceshop-223b3.firebasestorage.app',
+    messagingSenderId: '841092927417',
+    appId: '1:841092927417:web:c8c9dd9755ed4a76193068',
+  },
+};
